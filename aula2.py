dfc426dd12618005f0326953c9a32fa3bebd7ff9
Andre3Vinicius <138264069+Andre3Vinicius@users.noreply.github.com>
2024-09-25 09:55:34 -0300

    segundo commit

---
+++ aula2.py
@@ -0,0 +1,14 @@
+mensagem = 'eu adoro comida caseira'
+
+
+print(mensagem.lower())#todas as letras do texto em minuscula
+
+print(mensagem.upper())#todas as letras do texto em maiuscula
+
+print(mensagem.capitalize())#Primeira letra da frase maiuscula
+
+print(mensagem.find())#Sinaliza a posição da letra
+
+print(mensagem.replace('a', 'e'))#Troca a letra(palavra) por outra 
+ 
+print(mensagem.strip())#remove qualquer espaço que tenha antes do primeiro caractere
